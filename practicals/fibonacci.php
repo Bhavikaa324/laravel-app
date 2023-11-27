fb521fb4c64800ccac7d636e5711e63aadabbd10
Bhavikaa324 <puppalwarbhavika@gmail.com>
2023-11-27 19:52:33 +0530

    Updated fibonacci program for readability and maintainability

--- practicals/fibonacci.php
+++ practicals/fibonacci.php
@@ -1,12 +1,30 @@
 <?php
-$n = 10;
-$n1 = 0;
-$n2 = 1;
-echo $n1," ",$n2," ";
-for ($i = 2; $i <= $n; $i++){
-$n3 = $n1 + $n2;
-echo $n3," ";
-$n1 = $n2;
-$n2 = $n3;
+// Function to generate Fibonacci series up to a given limit
+function generateFibonacci($limit) {
+    // Initialize the first two numbers in the series
+    $num1 = 0;
+    $num2 = 1;
+
+    // Display the first two numbers
+    echo $num1 . ", " . $num2;
+
+    // Loop to generate the rest of the series
+    while ($num1 + $num2 <= $limit) {
+        // Calculate the next number in the series
+        $nextNum = $num1 + $num2;
+
+        // Display the next number
+        echo ", " . $nextNum;
+
+        // Update $num1 and $num2 for the next iteration
+        $num1 = $num2;
+        $num2 = $nextNum;
+    }
 }
+
+// Set the limit for the Fibonacci series
+$limit = 100;
+
+// Call the function to generate and display the Fibonacci series
+generateFibonacci($limit);
 ?>
