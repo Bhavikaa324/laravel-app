eec4dad5a8ecc3a6101dc7bd3badce3c25b4ad48
Bhavikaa324 <puppalwarbhavika@gmail.com>
2023-11-28 01:46:24 +0530

    Add a static HTML page to display songs

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
 // use Practicals\Song;
 use App\Models\Song;
-
-
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -24,24 +19,33 @@ Route::get('/hello', function () {
 Route::get('/veggies', function () {
     return view('table');
 });
+Route::get('/songs_static', function () {
+    $song1 = new Song();
+    $song1->setTitle("Stan");
+    $song1->setArtist("Eminem");
+    // $song1 = new Song();
+    // $song1->setTitle("Stan");
+    // $song1->setArtist("Eminem");
 
-    Route::get('/songs_static', function () {
-        $song1 = new Song();
-        $song1->setTitle("Stan");
-        $song1->setArtist("Eminem");
-      
-        $song2 = new Song();
-        $song2->setTitle("Nothing Else Matters");
-        $song2->setArtist("Metallica");
-      
-        $song3 = new Song();
-        $song3->setTitle("With You");
-        $song3->setArtist("A P Dhillon");
-    
-        return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
-      });
-    
-    
-      Route::get('/songs', function () {
-        return view('songs', [ 'songs' => Song::all() ] );
-    });
+    $song2 = new Song();
+    $song2->setTitle("Nothing Else Matters");
+    $song2->setArtist("Metallica");
+    // $song2 = new Song();
+    // $song2->setTitle("Nothing Else Matters");
+    // $song2->setArtist("Metallica");
+
+    $song3 = new Song();
+    $song3->setTitle("With You");
+    $song3->setArtist("A P Dhillon");
+    // $song3 = new Song();
+    // $song3->setTitle("With You");
+    // $song3->setArtist("A P Dhillon");
+
+    return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
+    return view('songs_static'); 
+  });
+
+
+  Route::get('/songs', function () {
+    return view('songs', [ 'songs' => Song::all() ] );
+});
