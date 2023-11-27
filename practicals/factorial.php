94e1ff1a67949ffd37030a5dc3ca19653f13118c
Bhavikaa324 <puppalwarbhavika@gmail.com>
2023-11-27 19:56:59 +0530

    Updated factorial program for readability and maintainability

--- practicals/factorial.php
+++ practicals/factorial.php
@@ -1,10 +1,28 @@
 <?php
-$n = 6;
-$fact = 1;
-while ($n > 0) {
-    $fact *=$n;
-    $n--;
+// Function to calculate the factorial of a number
+// Function to calculate the factorial 
+function factorial($n) {
+    if ($n < 0) {
+        return "Factorial is not defined for negative numbers.";
+    } elseif ($n == 0) {
+        return 1; // Factorial of 0 is 1
+    } else {
+        $result = 1;
+        for ($i = 1; $i <= $n; $i++) {
+            $result *= $i;
+        }
+        return $result;
+    }
 }
+// Number for which you want to calculate the factorial
+$number = 5; // Change this to the desired number
 
-echo $fact;
+if ($number >= 0) {
+    $fact = factorial($number);
+	$fact = factorial($number);
+	// Factorial of a number
+    echo "Factorial of $number is $fact";
+} else {
+    echo "Factorial is not defined for negative numbers.";
+}
 ?>
